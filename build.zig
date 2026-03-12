36b5cc6bdf9e33d24b8ca4f7473fa9deeeca6f30
Lukas Strnad <strnad.lukas@proton.me>
2026-03-12 14:36:56 +0100

    chore: New version of zig

--- build.zig
+++ build.zig
@@ -7,9 +7,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "cellular_automata",
-        .root_source_file = root_source_file,
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = root_source_file,
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     exe.linkSystemLibrary("sdl3");
@@ -29,11 +31,11 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_exe.step);
 
-    const exe_unit_tests = b.addTest(.{
+    const exe_unit_tests = b.addTest(.{ .root_module = b.createModule(.{
         .root_source_file = root_source_file,
         .target = target,
         .optimize = optimize,
-    });
+    }) });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
